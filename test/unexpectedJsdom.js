43bb17a3c4e871c1e217978ccd19dbadb69d01c8
Andreas Lind Petersen <andreas@one.com>
2014-05-20 00:31:50 +0200

    Initial commit.

---
+++ test/unexpectedJsdom.js
@@ -0,0 +1,22 @@
+var unexpected = require('unexpected'),
+    unexpectedJsdom = require('../lib/unexpectedJsdom'),
+    jsdom = require('jsdom'),
+    chalk = require('chalk');
+
+describe('unexpected-jsdom', function () {
+    var expect = unexpected.clone().installPlugin(unexpectedJsdom);
+
+    it('should consider two DOM elements equal when they have the same outerHTML', function () {
+        expect(jsdom.jsdom('<div>foobar</div>'), 'to equal', jsdom.jsdom('<div>foobar</div>'));
+    });
+
+    it('should consider two DOM elements different when their outerHTML values differ', function () {
+        var actualHtml = '<!DOCTYPE html><div>foobarbaz</div>',
+            expectedHtml = '<div class="hey">foobarquux</div><!--blahblah-->';
+        expect(function () {
+            expect(jsdom.jsdom(actualHtml), 'to equal', jsdom.jsdom(expectedHtml));
+        }, 'to throw exception', function (err) {
+            expect(chalk.stripColor(err.message), 'to equal', 'expected ' + actualHtml + ' to equal ' + expectedHtml);
+        });
+    });
+});
